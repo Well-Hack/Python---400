f1da6f49ade0718cc4a0740d28b54387c3966fe9
Well-Hack <wellersonsouza60@gmail.com>
2026-02-06 12:48:42 -0300

    up

---
+++ 005.py
@@ -0,0 +1,5 @@
+print('05 - Crie uma variavel nome e atribua para a mesma um nome'
+      'digitado pelo usuário: ')
+
+nome = str(input('Digita o seu nome: '))
+print(f'Olá {nome}!')
---
+++ 006.py
@@ -0,0 +1,5 @@
+print('6 - Exiba em tela o valor e o tipo de dado da variável num1:Sendo num1 = 1987 ')
+
+num1 = 1987
+if tipo == 
+print (f'O numero 1987 é do tipo {tipo}'type(num1))
